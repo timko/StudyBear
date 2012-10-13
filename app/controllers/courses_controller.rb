36d2f709c4a23db7ec8089e6dd2616c66795cb6f
saasbook <saasbook@saasbook.(none)>
2012-10-12 22:39:03 -0700

    forgot to add the redirection for guests

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,26 @@
 class CoursesController < ApplicationController
 
-  def index
+  def redirect_guest
     if !user_signed_in?
       render "home/logger" and return
     end
+  end
+
+  def index
+    redirect_guest
     @courses = Course.all
+  end
+
+  def new
+    redirect_guest
+  end
+
+  def create
+    course = Course.create(params[:course])
+    if course
+      flash[:notice] = "Course Successfully added"
+    end
+    redirect_to courses_path
   end
 
 end
