af21b7dfb3a358d2a7b00935cf812a0be8974557
saasbook <saasbook@saasbook.(none)>
2012-10-12 18:11:16 -0700

    first push

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StudyBear::Application.config.secret_token = 'f02ac6ef3cc645aa8176b97d8acc0d65669728de208a0ee82eecfd5083c7ec6c7ad62737ddc451878d3c74a151ddc40bcb748ac14ae9322e930d658cc57ee0f6'
