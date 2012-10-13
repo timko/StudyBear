ff7f6f81cb13b0ac5abb5d57e9a80996916a2f65
saasbook <saasbook@saasbook.(none)>
2012-10-12 20:18:46 -0700

    added topics model

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,6 @@ class Course < ActiveRecord::Base
   attr_accessible :course_num, :name
   validates :course_num, :presence => true, :uniqueness => true
   validates :name, :presence => true
+  has_many :follows
+  has_many :users, :through => :follows
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   validates :name, :presence => true
   validates :email, :presence => true, :uniqueness => true
   validates :password, :presence => true
+  has_many :follows
+  has_many :courses, :through => :follows
 end
